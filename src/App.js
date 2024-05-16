9fb93a32dbc6ec9dd2c292a94e1eea2ec4ed701a
SofyaGrischenko <sofya10293847@gmail.com>
2024-05-16 15:33:29 +0300

    ready steady go

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState} from 'react';
+import './Styles/app.css';
+import axios from 'axios';
+import Card from './components/Card';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [cards, setCards] = useState([])
+
+
+useEffect(() =>{
+//const response = 
+axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {setCards(response.data)})
+    .catch((error) => console.error(error))   
+  }, []);
+
+
+    return (
+      <>   
+      <div className="App"> 
+        {cards.map(user => 
+              <Card key = {user.id} props = {user}></Card>
+
+          )}
+        </div>
+      </>
+      
+      
+    );
+  }
 
 export default App;
